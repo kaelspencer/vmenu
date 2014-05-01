13bde3bb0c5c772ef2bd0d55bd55d83d23f7397e
kaelspencer <kaelspencer@gmail.com>
2014-04-30 22:42:00 -0700

    Implement Paging This adds a new class Paginator that handles paging. It can go to a letter offset or +- pages (or a combination of both).

---
+++ paginator.py
@@ -0,0 +1,37 @@
+class Paginator(object):
+    def __init__(self, objects, key, letter, offset, pagesize):
+        self.objects = objects
+        self.letter = letter.lower()
+        self.offset = offset
+        self.pagesize = pagesize
+        self.start = 0
+
+        for i, v in enumerate(objects):
+            try:
+                # Try object style first.
+                cur = getattr(v, key)[0]
+            except AttributeError:
+                # Now try dict.
+                cur = v.get(key)
+                if cur is None:
+                    raise AttributeError
+
+            cur = cur.lower()
+
+            if cur < letter:
+                self.start = i
+            elif cur == letter:
+                self.start = i
+                break
+
+        self.start += offset * pagesize
+
+        if self.start < 0:
+            self.start = 0
+        elif self.start >= len(objects):
+            self.start = len(objects) - len(objects) % pagesize
+
+        print 'Letter: %s, Offset: %s, Page Size: %s, Length: %s, Start: %s' % (letter, offset, pagesize, len(objects), self.start)
+
+    def page(self):
+        return self.objects[self.start:self.start + self.pagesize]
--- vmenu.py
+++ vmenu.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from werkzeug.routing import BaseConverter
 import evernote_wrapper, string
+from paginator import Paginator
 
 app = Flask(__name__)
 
@@ -33,20 +34,23 @@ app.jinja_env.globals.update(footer_links=footer_links)
 @app.route('/<regex("[a-zA-Z]{1}"):start>/')
 @app.route('/<regex("-?[0-9]*"):page>/')
 @app.route('/<regex("[a-zA-Z]{1}"):start>/<regex("-?[0-9]*"):page>/')
-def show_tags(start='', page=0):
+def show_tags(start='a', page=0):
     tags = evernote_wrapper.get_tags()
+    paginator = Paginator(tags, 'name', start, int(page), 6)
     start += '/'
-    return render_template('tags.html', tags=tags, tagurl='/', start=start, page=int(page))
+    return render_template('tags.html', tags=paginator.page(), tagurl='/', start=start, page=int(page))
 
 @app.route('/tag/<tag>/')
 @app.route('/tag/<tag>/<regex("[a-zA-Z]{1}"):start>/')
 @app.route('/tag/<tag>/<regex("[0-9]*"):page>/')
-@app.route('/tag/<tag>/<regex("[a-zA-Z]{1}"):start>/<regex("[0-9]*"):page>/')
+@app.route('/tag/<tag>/<regex("[a-zA-Z]{1}"):start>/<regex("-?[0-9]*"):page>/')
 def show_recipes(tag, start='a', page=0):
     recipes = evernote_wrapper.get_recipes(tag)
+    print "%s recipes" % len(recipes)
+    paginator = Paginator(recipes, 'title', start, int(page), 6)
     tagurl = '/tag/%s/' % tag
     start += '/'
-    return render_template('recipes.html', recipes=recipes[:6], tagurl=tagurl, start=start, page=page)
+    return render_template('recipes.html', recipes=paginator.page(), tagurl=tagurl, start=start, page=int(page))
 
 @app.route('/recipe/<recipe>/')
 def show_recipe(recipe):
