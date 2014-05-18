2ca9120edaa0200d59d9f66e9b6abcbd39ddb80f
Kael Spencer <kael@euler>
2014-05-17 17:21:13 -0700

    Add Scrolling to Recipe Page Add a footer with scroll buttons. There is a fixed height article with the overflow hidden. The buttons change margin-top for the inner div wrapper.

---
+++ static/scroll.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    scrollUp = document.getElementById('scroll-up');
+    scrollDown = document.getElementById('scroll-down');
+    scroller = document.getElementById('scroller');
+    if (scrollUp === null || scrollDown === null || scroller === null) {
+        throw "Can't find the correct objects.";
+    }
+    delta = 300;
+
+    scrollUp.onclick = function() {
+        scroll(scroller, delta);
+        return false;
+    };
+
+    scrollDown.onclick = function() {
+        scroll(scroller, -1 * delta);
+        return false;
+    };
+
+
+};
+
+function scroll(scroller, delta) {
+    cur = scroller.style.marginTop === '' ? 0 : parseInt(scroller.style.marginTop);
+    cur += delta;
+    if (cur > 0){
+        cur = 0;
+    }
+
+    scroller.style.marginTop = cur;
+}
