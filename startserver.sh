3ebad1013303675156c99d19e1301835abfb0059
Kael Spencer <kaelspencer@gmail.com>
2014-09-27 14:48:14 -0700

    Change Startup Script to be Compatible with Supervisord
    Supervisord requires the script to not run, so this change causes
    uWSGI to not run as a daemon.

--- startserver.sh
+++ startserver.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 PROJDIR=$PWD
-PIDFILE="$PROJDIR/vmenu.pid"
-
-if [ -f $PIDFILE ]; then
-    kill `cat -- $PIDFILE`
-    rm -f -- $PIDFILE
-fi
 
 . ../virtualenv/vmenu/bin/activate
 
 export VMENU_SETTINGS=debug.cfg
 #exec celery --app=vmenu.celery worker --logfile /var/log/celery.log &
-exec uwsgi -s 127.0.0.1:49152 -w vmenu:app --daemonize /var/log/uwsgi/vmenu.log --pidfile $PIDFILE
+exec uwsgi -s 127.0.0.1:49152 -w vmenu:app --logto /var/log/uwsgi/vmenu.log
