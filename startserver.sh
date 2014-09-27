199d1a889c50b0d497736c95086d92bbbfb35efb
Kael Spencer <kaelspencer@gmail.com>
2014-09-27 11:07:19 -0700

    Add VirtualEnv to Startup Script
    Add dot-sourcing of the virtualenv to the startup script.

--- startserver.sh
+++ startserver.sh
@@ -8,6 +8,8 @@ if [ -f $PIDFILE ]; then
     rm -f -- $PIDFILE
 fi
 
+. ../virtualenv/vmenu/bin/activate
+
 export VMENU_SETTINGS=debug.cfg
-exec celery --app=vmenu.celery worker --logfile /var/log/celery.log &
+#exec celery --app=vmenu.celery worker --logfile /var/log/celery.log &
 exec uwsgi -s 127.0.0.1:49152 -w vmenu:app --daemonize /var/log/uwsgi/vmenu.log --pidfile $PIDFILE
